5f3fcb368d123c08c7a4bba6253bb8afd948e864
Julio Zittei <julio.dutra@bitz.com.br>
2023-04-12 18:02:03 -0300

    Add Tasks component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
+import { Tasks } from './components/Tasks';
+
 function App(): JSX.Element {
-  const [tasks, setTasks] = useState([]);
-
-  const fetchTasks = async () => {
-    try {
-      const { data } = await axios.get(`https://fsc-task-manager-backend.herokuapp.com/tasks`);
-      setTasks(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchTasks();
-  }, []);
-
-  return <div className="App">teste</div>;
+  return (
+    <>
+      <Tasks />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Tasks.tsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import './Tasks.scss';
+
+type Props = {};
+
+type Task = {
+  id: string;
+  description: string;
+  isCompleted: boolean;
+};
+
+export function Tasks(props: Props) {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const fetchTasks = async () => {
+    try {
+      const { data } = await axios.get(`https://fsc-task-manager-backend.herokuapp.com/tasks`);
+      setTasks(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  return (
+    <div className="task-container">
+      <h2>Minhas tarefas</h2>
+
+      <div className="last-tasks">
+        <h3>Últimas tarefas</h3>
+        <div className="task-list">
+          {tasks
+            .filter((task) => !task.isCompleted)
+            .map((task) => {
+              return <div>task</div>;
+            })}
+        </div>
+      </div>
+
+      <div className="completed-tasks">
+        <h3>Tarefas concluídas</h3>
+        <div className="task-list">
+          {tasks
+            .filter((task) => task.isCompleted)
+            .map((task) => {
+              return <div>task</div>;
+            })}
+        </div>
+      </div>
+    </div>
+  );
+}
